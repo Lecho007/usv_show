eb746c5376e9b23970a6c4bb0ba873e6c6460cbb
Echo <2543293454@qq.com>
2025-11-03 17:38:48 +0800

    加入 mqtt,修改app.py的接口

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import time
 import pymysql
+import threading, paho.mqtt.client as mqtt, json
 
 app = Flask(__name__)
+latest_data = None  # 用于存储最新的 MQTT 消息内容（坐标信息）。
 
 # 配置 MySQL 数据库连接
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://usvuser:usvpass@mysql:3306/usvdb'
@@ -42,10 +44,41 @@ with app.app_context():
     db.create_all()
     print("Tables created (if not exist)")
 
+
+"""
+  通过 MQTT 接收 RTK 模块上传的定位数据，并提供一个 Flask HTTP API /api/position 来访问这些数据。  
+  RTK → 上传数据 → mqtt.wit-motion.cn
+  Flask + MQTT 客户端 → 订阅消息 → 保存最新定位
+  其他前端或系统 → 调用 /api/position → 拿到实时坐标
+"""
+# 当 MQTT 客户端接收到新的消息时，这个函数会被自动调用。
+def on_message(client, userdata, msg):
+    global latest_data
+    latest_data = msg.payload.decode()  #消息（msg.payload）是二进制的，所以需要 .decode() 转成字符串。
+
+# 启动 MQTT 客户端线程
+def mqtt_thread():
+    client = mqtt.Client()
+    client.username_pw_set("CYL", "FDA4235JHKBFSJF541KDO3NFK4")
+    client.on_message = on_message
+    client.connect("mqtt.wit-motion.cn", 31883, 60)
+    client.subscribe("device/868327071852022/upload")
+    client.loop_forever()
+
+# 其他前端或系统 → 调用 /api/position → 拿到实时坐标
+@app.route("/api/position")
+def get_position():
+    if latest_data:
+        return jsonify(json.loads(latest_data))
+    else:
+        return jsonify({"status": "waiting for data"}), 404
+
+
 # 测试路由
 @app.route('/')
 def hello():
     return 'Hello Flask + MySQL!'
+
 
 if __name__ == '__main__':
     # 容器内监听 0.0.0.0
---
+++ backend/mqtt/mqtt_data_saver.py
@@ -0,0 +1,47 @@
+﻿import paho.mqtt.client as mqtt
+
+# MQTT配置
+broker_address = "mqtt.wit-motion.cn"
+broker_port = 31883
+username = "CYL"
+password = "FDA4235JHKBFSJF541KDO3NFK4"
+
+subtopic_list = {
+    "device/868327071852022/upload",
+}
+
+def save_message_to_file(filename, message):
+    with open(filename, "ab") as f:
+        f.write(message)
+    print(f"消息已保存到 {filename}")
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    for subtopic in subtopic_list:
+        client.subscribe(subtopic)
+
+def on_message(client, userdata, msg):
+    try:
+        print(f"Received: '{msg.payload}' on topic: {msg.topic}")
+        filename = msg.topic.replace('/', '_')+'.txt'
+        save_message_to_file(filename, msg.payload)
+    except Exception as e:
+        print(f"处理消息出错: {e}")
+
+def main():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.username_pw_set(username, password)
+    
+    try:
+        client.connect(broker_address, broker_port, 60)
+        print(f"已连接至 {broker_address}:{broker_port}")
+        client.loop_forever()
+    except Exception as e:
+        print(f"连接失败: {e}")
+    finally:
+        client.disconnect()
+
+if __name__ == "__main__":
+    main()
